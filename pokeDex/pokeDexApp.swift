d1cbd0044bdfc92f24d245f231b3a2a18def9569
Raveendran <ravindran1307@gmail.com>
2021-06-29 23:13:53 +0530

    Initial Commit

---
+++ pokeDex/pokeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokeDexApp.swift
+//  pokeDex
+//
+//  Created by Raveendran on 29/06/21.
+//
+
+import SwiftUI
+
+@main
+struct pokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
